fd90657a8d2e6298b520dc8fa83430da1b21a0ff
Steve Otteson <sotteson@xoanan.com>
2020-08-19 08:12:52 -0700

    Exit with 0 since the generator returns non-0 for warnings

--- scripts/GenerateMetadataSource.ps1
+++ scripts/GenerateMetadataSource.ps1
@@ -115,11 +115,9 @@ Replace-Text $fixedSettingsRsp $textToReplaceTable
 Write-Host "Creating metdata .cs file. Log output: $generatorOutput"
 Write-Host "Calling: $toolsDir\ClangSharpPInvokeGenerator.exe @$generateDir\remap.rsp @$fixedSettingsRsp @$libMappingOutputFileName 2>&1 > $generatorOutput"
 
-$oldLoc = Get-Location
-Set-Location $toolsDir
 & $toolsDir\ClangSharpPInvokeGenerator.exe "@$generateDir\remap.rsp" "@$fixedSettingsRsp" "@$libMappingOutputFileName" 2>&1 > $generatorOutput
-Set-Location $oldLoc
 
 $missedFuncsOutput = Join-Path -Path $generationOutArtifactsDir -ChildPath "missedfuncs.output.txt"
 & $PSScriptRoot\CheckMissedFuncs.ps1 -generatorResults $generatorOutput -mappingFile $libMappingOutputFileName -missedFuncsFile $missedFuncsOutput
 
+exit 0
