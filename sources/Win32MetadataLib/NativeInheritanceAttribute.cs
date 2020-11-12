20629624252e4ef4275fe02c954133dbb0b8d510
Steve Otteson <sotteson@xoanan.com>
2020-11-11 16:23:03 -0800

    Add NativeInheritanceAttribute to show the native type a type inherited from

---
+++ sources/Win32MetadataLib/NativeInheritanceAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics;
+
+namespace Microsoft.Windows.Sdk
+{
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+    public sealed class NativeInheritanceAttribute : Attribute
+    {
+        public NativeInheritanceAttribute(string baseName)
+        {
+            this.BaseName = BaseName;
+        }
+
+        public string BaseName { get; }
+    }
+}
