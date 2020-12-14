1b669375ad63d21cbc8a80f6f1c510a24b2a3034
Andrew Arnott <andrewarnott@gmail.com>
2020-12-13 17:00:05 -0700

    Replace in-repo CsWin32 with test of external one

---
+++ sources/CsWin32/BasicTests.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Windows.Sdk;
+
+public class BasicTests
+{
+    public void GetTickCount_Nonzero()
+    {
+        uint result = PInvoke.GetTickCount();
+    }
+}
