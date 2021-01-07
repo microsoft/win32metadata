f65fdeaae92866ce06699e122f7b1794f58064fe
Steve Otteson <sotteson@xoanan.com>
2021-01-06 22:58:55 -0800

    Missed Backup partition because .gitignore was ignoring it

---
+++ generation/Partitions/Backup/main.cpp
@@ -0,0 +1,13 @@
+// #define SECURITY_WIN32 // For sspi.h
+
+#include "intrinfix.h"
+
+#include <windows.h>
+#include <sdkddkver.h>
+
+// #include <winbase.h>
+//#include <windows.h>
+
+//#define _AMD64_
+
+#include <winnt.h>
