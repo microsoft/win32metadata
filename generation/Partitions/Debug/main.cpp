feeb054328f077174501c59b961f5f78621d8401
Steve Otteson <sotteson@xoanan.com>
2021-01-06 23:35:19 -0800

    Missed debug partition due to .gitignore

---
+++ generation/Partitions/Debug/main.cpp
@@ -0,0 +1,11 @@
+#define SECURITY_WIN32 // For sspi.h
+#define QCC_OS_GROUP_WINDOWS
+
+#include "intrinfix.h"
+
+#include <windows.h>
+#include <sdkddkver.h>
+
+#include <activdbg.h>
+#include <activprof.h>
+#include <webapplication.h>
