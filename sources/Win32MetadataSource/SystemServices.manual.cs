cdf514c02f4567d574d722b9c27d3037d62e615f
Saul Rennison <saul.rennison@gmail.com>
2021-01-23 12:24:22 +0000

    Add ConsoleMode flags enum

--- sources/Win32MetadataSource/SystemServices.manual.cs
+++ sources/Win32MetadataSource/SystemServices.manual.cs
@@ -5892,4 +5892,22 @@ namespace Windows.Win32.SystemServices
 
         public const uint INFINITE = 0xFFFFFFFF;
     }
+
+    [Flags]
+    enum ConsoleMode
+    {
+        ENABLE_ECHO_INPUT = 0x0004,
+        ENABLE_INSERT_MODE = 0x0020,
+        ENABLE_LINE_INPUT = 0x0002,
+        ENABLE_MOUSE_INPUT = 0x0010,
+        ENABLE_PROCESSED_INPUT = 0x0001,
+        ENABLE_QUICK_EDIT_MODE = 0x0040,
+        ENABLE_WINDOW_INPUT = 0x0008,
+        ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200,
+        ENABLE_PROCESSED_OUTPUT = 0x0001,
+        ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002,
+        ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004,
+        DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
+        ENABLE_LVB_GRID_WORLDWIDE = 0x0010
+    }
 }
