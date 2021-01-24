b2e66364ed2f64112ff1faf63998c987c9f47869
Morten Nielsen <1378165+dotMorten@users.noreply.github.com>
2021-01-23 19:47:04 -0800

    Fix commandline script
    Not sure why this says `pwsh.exe`. That didn't work on my PC, so wonder if it's a local tool you have? The other scripts says `powershell.exe` and it works for me after changing it.

--- GenerateMetadataSource.cmd
+++ GenerateMetadataSource.cmd
@@ -1,3 +1,3 @@
 @echo OFF
-pwsh.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0scripts\GenerateMetadataSource.ps1""" %*"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0scripts\GenerateMetadataSource.ps1""" %*"
 exit /B %ERRORLEVEL%
