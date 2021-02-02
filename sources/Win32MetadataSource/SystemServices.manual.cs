d6e42b9b0011fe771a08d2e9a605af2abbd31f30
Steve Otteson <sotteson@xoanan.com>
2021-02-02 11:27:10 -0800

    Change the namespace of a new flag to match the APIs and also fix a build break.

--- sources/Win32MetadataSource/SystemServices.manual.cs
+++ sources/Win32MetadataSource/SystemServices.manual.cs
@@ -5910,4 +5910,22 @@ namespace Windows.Win32.SystemServices
         DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
         ENABLE_LVB_GRID_WORLDWIDE = 0x0010
     }
+
+    [Flags]
+    public enum PAGE_TYPE : uint
+    {
+        PAGE_NOACCESS = 0x1,
+        PAGE_READONLY = 0x2,
+        PAGE_READWRITE = 0x4,
+        PAGE_WRITECOPY = 0x8,
+        PAGE_EXECUTE = 0x10,
+        PAGE_EXECUTE_READ = 0x20,
+        PAGE_EXECUTE_READWRITE = 0x40,
+        PAGE_EXECUTE_WRITECOPY = 0x80,
+        PAGE_GUARD = 0x100,
+        PAGE_NOCACHE = 0x200,
+        PAGE_WRITECOMBINE = 0x400,
+        PAGE_TARGETS_INVALID = 0x40000000,
+        PAGE_TARGETS_NO_UPDATE = 0x40000000
+    }
 }
