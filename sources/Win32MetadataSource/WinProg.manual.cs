f0f32b0fbb1126423aa60d57104a401e12e89a95
Steve Otteson <sotteson@xoanan.com>
2021-02-12 11:27:05 -0800

    OpenProcess should use an enum for dwDesiredAccess

--- sources/Win32MetadataSource/WinProg.manual.cs
+++ sources/Win32MetadataSource/WinProg.manual.cs
@@ -71,30 +71,4 @@ namespace Windows.Win32.WindowsProgramming
         VER_SUITENAME = 0x0000040,
         VER_PRODUCT_TYPE = 0x0000080
     }
-
-    [Flags]
-    public enum ProcessAccessRights : uint
-    {
-        Terminate = (0x0001),
-        CreateThread = (0x0002),
-        SetSessionid = (0x0004),
-        VmOperation = (0x0008),
-        VmRead = (0x0010),
-        VmWrite = (0x0020),
-        DupHandle = (0x0040),
-        CreateProcess = (0x0080),
-        SetQuota = (0x0100),
-        SetInformation = (0x0200),
-        QueryInformation = (0x0400),
-        SuspendResume = (0x0800),
-        QueryLimitedInformation = (0x1000),
-        SetLimitedInformation = (0x2000),
-        AllAccess = (StandardRightsRequired | Synchronize | 0xFFFF),
-        Delete = (0x00010000),
-        ReadControl = (0x00020000),
-        WriteDac = (0x00040000),
-        WriteOwner = (0x00080000),
-        Synchronize = (0x00100000),
-        StandardRightsRequired = (0x000F0000)
-    }
 }
