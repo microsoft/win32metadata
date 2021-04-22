ee499c40eb306cfc3616c17231cd0e417ff9c813
Mike Battista <mbattist@microsoft.com>
2021-04-22 16:17:00 -0700

    Renamed Direct*.

--- generation/emitter/manual/Direct3D11.manual.cs
+++ generation/emitter/manual/Direct3D11.manual.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Windows.Win32.Direct3D11
+namespace Windows.Win32.Graphics.Direct3D11
 {
     public static unsafe partial class Apis
     {
