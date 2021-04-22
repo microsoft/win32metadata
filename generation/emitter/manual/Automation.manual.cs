5d4b4180cd69c92a6904261ba30f7e047aa3f4c1
Mike Battista <mbattist@microsoft.com>
2021-04-22 16:04:58 -0700

    Renamed AllJoyn-DeviceAndDriverInstallation.

--- generation/emitter/manual/Automation.manual.cs
+++ generation/emitter/manual/Automation.manual.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 using Windows.Win32.Com;
 
-namespace Windows.Win32.Automation
+namespace Windows.Win32.System.OleAutomation
 {
     // For EXCEPINFO::pfnDeferredFillIn which defines the callback inline with the struct.
     // ClangSharp emits function pointers as IntPtr so we need to write our own signature
