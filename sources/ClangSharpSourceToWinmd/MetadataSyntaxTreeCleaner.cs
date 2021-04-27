86cb5c17a97c20658d90403c0044a5c5c44844c0
Steve Otteson <sotteson@xoanan.com>
2021-04-26 20:40:12 -0700

    Fixes #244, fixes #422 - Add DoNotReleaseAttribute for handles that are not meant to be released Fixes #441 - Windows 8.1 wasn't being used for SupportedOSPlatform Fixes #424 - LoadLibraryA and friends needs to return a handle type that is freed with FreeLibrary Fixes #421 - HINSTANCE says to free with FreeLibrary but the latter expects an IntPtr not an HINSTANCE

--- sources/ClangSharpSourceToWinmd/MetadataSyntaxTreeCleaner.cs
+++ sources/ClangSharpSourceToWinmd/MetadataSyntaxTreeCleaner.cs
@@ -347,6 +347,10 @@ namespace ClangSharpSourceToWinmd
                 }
 
                 string fullName = SyntaxUtils.GetFullName(node);
+                if (fullName == "GetModuleHandleA")
+                {
+
+                }
 
                 // Remove duplicate static methods
                 if (node.Body == null)
@@ -859,11 +863,14 @@ namespace ClangSharpSourceToWinmd
             private bool GetRemapInfo(string fullName, out List<AttributeSyntax> listAttributes, string currentType, out string newType, out string newName)
             {
                 listAttributes = null;
+                newType = null;
                 newName = null;
+
+                bool ret = false;
 
                 if (!string.IsNullOrEmpty(fullName) && this.remaps.TryGetValue(fullName, out string remapData))
                 {
-                    var ret = EncodeHelpers.DecodeRemap(remapData, out listAttributes, out newType, out newName);
+                    ret = EncodeHelpers.DecodeRemap(remapData, out listAttributes, out newType, out newName);
                     if (newType != null)
                     {
                         // Try to keep the pointers at the same level if we're replacing
@@ -879,10 +886,9 @@ namespace ClangSharpSourceToWinmd
                             }
                         }
                     }
-
-                    return ret;
                 }
-                else
+
+                if (newType == null)
                 {
                     // See if the type ends in the magic suffix. We use a remap fed to clangsharp 
                     // to keep some typedefs from following down to their original type
@@ -892,14 +898,11 @@ namespace ClangSharpSourceToWinmd
                     if (typeOnly.EndsWith(RevertToTypeSuffix))
                     {
                         newType = typeOnly.Substring(0, typeOnly.Length - RevertToTypeSuffix.Length) + pointers;
-                        return true;
+                        ret = true;
                     }
                 }
 
-                newType = null;
-                newName = null;
-
-                return false;
+                return ret;
             }
         }
 
