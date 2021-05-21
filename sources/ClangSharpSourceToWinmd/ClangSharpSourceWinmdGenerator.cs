bd35dcd535c0960b2287d3d701a887252e95828c
Mike Battista <mbattist@microsoft.com>
2021-05-21 14:29:43 -0700

    Moved common types to .Foundation.

--- sources/ClangSharpSourceToWinmd/ClangSharpSourceWinmdGenerator.cs
+++ sources/ClangSharpSourceToWinmd/ClangSharpSourceWinmdGenerator.cs
@@ -22,8 +22,8 @@ namespace ClangSharpSourceToWinmd
 {
     public class ClangSharpSourceWinmdGenerator
     {
-        public const string Win32WideStringType = "Windows.Win32.System.SystemServices.PWSTR";
-        public const string Win32StringType = "Windows.Win32.System.SystemServices.PSTR";
+        public const string Win32WideStringType = "Windows.Win32.Foundation.PWSTR";
+        public const string Win32StringType = "Windows.Win32.Foundation.PSTR";
 
         private const string InteropNamespace = "Windows.Win32.Interop";
         private const string ScannedSuffix = "__scanned__";
