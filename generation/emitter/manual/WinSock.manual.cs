82daa3ede28af1ec0cfaeac45582e24e02bdcd7c
Mike Battista <mbattist@microsoft.com>
2021-06-11 12:08:08 -0700

    Added INVALID_SOCKET.

---
+++ generation/emitter/manual/WinSock.manual.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Interop;
+
+namespace Windows.Win32.Networking.WinSock
+{
+    public static unsafe partial class Apis
+    {
+        // (SOCKET)(~0) which equals uint.MaxValue
+        public const uint INVALID_SOCKET = uint.MaxValue;
+    }
+}
