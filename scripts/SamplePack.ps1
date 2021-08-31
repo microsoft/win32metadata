1e998d5a5364959c77db4fef1fd993308f418165
Andrew Arnott <andrewarnott@live.com>
2021-08-31 15:31:34 -0600

    Delete obsolete sample pack script

--- scripts/SamplePack.ps1
+++
@@ -1,2 +0,0 @@
-.\tools\nuget.exe pack .\sources\nuget\Microsoft.Windows.SDK.Win32Metadata\Microsoft.Windows.SDK.Win32Metadata.nuspec -version 10.0.20195.1001-preview.rs-onecore-dep -BasePath . -verbosity Detailed -OutputDirectory $env:TEMP
-
