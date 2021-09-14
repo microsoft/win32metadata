microsoft/win32metadata

commit 931fa65996fef1d6139bd2fe7e4ddc59a8fb7e02

Build authoring cleanup (#630)
* Don't launch debugger during build unless an environment variable is set

* Add .editorconfig and apply to msbuild files

* Delete obsolete sample pack script

* Suppress some pack warnings

* Build the metadata package to the same path as docs by default

* Lots of build authoring cleanup

* Consolidate nuget.config files wherever possible.
* Consolidate msbuild sdk versions into a global.json file.
* Consolidate definitions of OutputPath in msbuild projects.
* Remove/fix msbuild properties that were inconsistently or redundantly applied in project files.
* Move Project Sdk attributes from Sdk.props to each project that imported it.
* Remove a bunch of 1-project solution files in favor of a new BuildTools.sln that contains them all.

* Target net5.0 instead of netcoreapp3.1

* Add task to print out .NET version info

* Disable signing consistently

* Set up VS env vars

* Fix bad paths for winmd

Directory.Build.props
@@ -11,12 +11,27 @@
   <PropertyGroup>
     <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
     <RepoRootPath>$(MSBuildThisFileDirectory)</RepoRootPath>
+    <BaseIntermediateOutputPath>$(RepoRootPath)obj\$(MSBuildProjectName)\</BaseIntermediateOutputPath>
+    <OutputPath>$(RepoRootPath)bin\$(Configuration)</OutputPath>
+    <PackageOutputPath>$(MSBuildThisFileDirectory)bin\Packages\$(Configuration)\NuGet\</PackageOutputPath>
+
+    <TaskBinDir>$(RepoRootPath)bin\$(Configuration)\netstandard2.0\</TaskBinDir>
+
+    <LangVersion>9</LangVersion>
+
+    <Company>Microsoft</Company>
+    <Authors>Microsoft</Authors>
+    <Copyright>© Microsoft Corporation. All rights reserved.</Copyright>
+    <PackageProjectUrl>https://github.com/microsoft/win32metadata</PackageProjectUrl>
+    <PackageRequireLicenseAcceptance>true</PackageRequireLicenseAcceptance>
+    <PackageIcon>images/windows.png</PackageIcon>
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include="Nerdbank.GitVersioning" Condition="!Exists('packages.config')">
-      <Version>3.4.194</Version>
-      <PrivateAssets>all</PrivateAssets>
-    </PackageReference>
+    <None Include="$(RepoRootPath)images/windows.png" Pack="true" PackagePath="images/" />
+  </ItemGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Nerdbank.GitVersioning" Version="3.4.231" PrivateAssets="all" />
   </ItemGroup>
 </Project>

azure-pipelines.yml
@@ -21,10 +21,10 @@ jobs:
     lfs: true
 
   - task: UseDotNet@2
-    displayName: Use DotNet 3.x
+    displayName: ⚙ Install .NET SDK
     inputs:
-      packageType: 'sdk'
-      version: '3.x'
+      packageType: sdk
+      useGlobalJson: true
 
   - task: PowerShell@2
     displayName: Set build version
@@ -70,10 +70,10 @@ jobs:
   - checkout: self
     lfs: true
   - task: UseDotNet@2
-    displayName: Use DotNet 3.x
+    displayName: ⚙ Install .NET SDK
     inputs:
-      packageType: 'sdk'
-      version: '3.x'
+      packageType: sdk
+      useGlobalJson: true
 
   - task: PowerShell@2
     displayName: GenerateMetadataSource.ps1 - x86
@@ -97,10 +97,10 @@ jobs:
   - checkout: self
     lfs: true
   - task: UseDotNet@2
-    displayName: Use DotNet 3.x
+    displayName: ⚙ Install .NET SDK
     inputs:
-      packageType: 'sdk'
-      version: '3.x'
+      packageType: sdk
+      useGlobalJson: true
 
   - task: PowerShell@2
     displayName: GenerateMetadataSource.ps1 - arm64
@@ -127,17 +127,17 @@ jobs:
   - checkout: self
     lfs: true
 
-  - task: UseDotNet@2
-    displayName: Use DotNet 3.x
+  - task: BatchScript@1
+    displayName: Set up VS environment
     inputs:
-      packageType: 'sdk'
-      version: '3.x'
+      filename: 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat'
+      modifyEnvironment: true
 
   - task: UseDotNet@2
-    displayName: Use DotNet 5.x
+    displayName: ⚙ Install .NET SDK
     inputs:
-      packageType: 'sdk'
-      version: '5.x'
+      packageType: sdk
+      useGlobalJson: true
 
     # ESRP Authenticode sign package DLLs
   - task: UseDotNet@2
@@ -145,6 +145,9 @@ jobs:
     inputs:
       packageType: runtime
       version: 2.1.x
+
+  - script: dotnet --info
+    displayName: Display .NET SDK/runtime info
 
   - task: DownloadPipelineArtifact@2
     displayName: Download x64 scraper obj assets
@@ -230,11 +233,53 @@ jobs:
     condition: and(succeeded(), eq(variables['SignFiles'], 'true'), ne(variables['Build.Reason'], 'PullRequest'))
 
   - task: EsrpCodeSigning@1
-    displayName: 'Authenticode Sign Binaries in MSBuild SDK package'
+    displayName: Authenticode sign net5.0 binaries in MSBuild SDK package
     inputs:
       ConnectedServiceName: 'Undocked RegFree Signing Connection'
-      FolderPath: '$(Build.SourcesDirectory)\bin\release\netcoreapp3.1'
-      Pattern: 'ClangSharpSourceToWinmd.dll,ConstantsScraper.dll,MetadataTasks.dll,MetadataUtils.dll,WinmdUtils.dll'
+      FolderPath: '$(Build.SourcesDirectory)\bin\$(BuildConfiguration)\net5.0'
+      Pattern: 'ClangSharpSourceToWinmd.dll,ConstantsScraper.dll,MetadataUtils.dll,WinmdUtils.dll'
+      signConfigType: 'inlineSignParams'
+      inlineOperation: |
+        [
+          {
+            "keyCode": "CP-230012",
+            "operationSetCode": "SigntoolSign",
+            "parameters": [
+              {
+                "parameterName": "OpusName",
+                "parameterValue": "Microsoft"
+              },
+              {
+                "parameterName": "OpusInfo",
+                "parameterValue": "http://www.microsoft.com"
+              },
+              {
+                "parameterName": "PageHash",
+                "parameterValue": "/NPH"
+              },
+              {
+                "parameterName": "FileDigest",
+                "parameterValue": "/fd sha256"
+              },
+              {
+                "parameterName": "TimeStamp",
+                "parameterValue": "/tr \"http://rfc3161.gtm.corp.microsoft.com/TSS/HttpTspServer\" /td sha256"
+              }
+            ],
+            "toolName": "signtool.exe",
+            "toolVersion": "6.2.9304.0"
+          }
+        ]
+      SessionTimeout: '60'
+      MaxConcurrency: '50'
+      MaxRetryAttempts: '2'
+    condition: and(succeeded(), eq(variables['SignFiles'], 'true'), ne(variables['Build.Reason'], 'PullRequest'))
+  - task: EsrpCodeSigning@1
+    displayName: Authenticode sign netstandard2.0 binaries in MSBuild SDK package
+    inputs:
+      ConnectedServiceName: 'Undocked RegFree Signing Connection'
+      FolderPath: '$(Build.SourcesDirectory)\bin\$(BuildConfiguration)\netstandard2.0'
+      Pattern: 'MetadataTasks.dll'
       signConfigType: 'inlineSignParams'
       inlineOperation: |
         [
@@ -361,7 +406,6 @@ jobs:
     inputs:
       packageType: sdk
       useGlobalJson: true
-      workingDirectory: apidocs
 
   # ESRP Authenticode sign package DLLs
   - task: UseDotNet@2

sources/msbuild/GeneratorSampleDebug/GeneratorSampleDebug.proj
@@ -1,40 +1,39 @@
 <?xml version="1.0" encoding="utf-8"?>
-<Project>
-    
-    <!-- These are for debugging purposes only. A normal project wouldn't set these -->
-    <PropertyGroup>
-        <TaskBinDir>$(MSBuildThisFileDirectory)..\..\..\bin\release\netcoreapp3.1</TaskBinDir>
-        <ToolsBinDir>$(MSBuildThisFileDirectory)..\..\..\bin\release\netcoreapp3.1</ToolsBinDir>
-        <LibToolsBinDir>$(MSBuildThisFileDirectory)..\..\..\tools</LibToolsBinDir>
-        <ScriptsDir>$(MSBuildThisFileDirectory)..\..\..\scripts</ScriptsDir>
-        <Win32MetadataScraperAssetsDir>$(MSBuildThisFileDirectory)..\..\..\generation\scraper</Win32MetadataScraperAssetsDir>
-        <Win32MetadataScraperGeneratedAssetsDir>$(MSBuildThisFileDirectory)..\..\..\generation\scraper\obj\x64</Win32MetadataScraperGeneratedAssetsDir>
-		<Win32WinmdBinDir>$(MSBuildThisFileDirectory)..\..\..\bin</Win32WinmdBinDir>
-    </PropertyGroup>
+<Project Sdk="Microsoft.Build.NoTargets">
 
-    <Import Project="..\sdk\sdk.props" />
+  <!-- These are for debugging purposes only. A normal project wouldn't set these -->
+  <PropertyGroup>
+    <ToolsBinDir>$(RepoRootPath)bin\$(Configuration)\net5.0</ToolsBinDir>
+    <LibToolsBinDir>$(RepoRootPath)tools</LibToolsBinDir>
+    <ScriptsDir>$(RepoRootPath)scripts</ScriptsDir>
+    <Win32MetadataScraperAssetsDir>$(RepoRootPath)generation\scraper</Win32MetadataScraperAssetsDir>
+    <Win32MetadataScraperGeneratedAssetsDir>$(RepoRootPath)generation\scraper\obj\x64</Win32MetadataScraperGeneratedAssetsDir>
+    <Win32WinmdBinDir>$(RepoRootPath)bin</Win32WinmdBinDir>
+  </PropertyGroup>
 
-    <PropertyGroup>
-        <OutputWinmd>bin\Windows.Win32.Graphics.DirectWriteCore.winmd</OutputWinmd>
-        <WinmdVersion>1.0.0.0</WinmdVersion>
-        <GenerateAssemblyVersionInfo>false</GenerateAssemblyVersionInfo>
-    </PropertyGroup>
+  <Import Project="..\sdk\sdk.props" />
 
-    <ItemGroup>
-        <DWriteHeaders Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.h"/>
-        <DWriteLibs Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.lib"/>
+  <PropertyGroup>
+    <OutputWinmd>bin\Windows.Win32.Graphics.DirectWriteCore.winmd</OutputWinmd>
+    <WinmdVersion>1.0.0.0</WinmdVersion>
+    <GenerateAssemblyVersionInfo>false</GenerateAssemblyVersionInfo>
+  </PropertyGroup>
 
-        <ImportLibs Include="@(DWriteLibs)"/>
-        <Partition Include="main.cpp">
-            <TraverseFiles>@(DWriteHeaders)</TraverseFiles>
-            <Namespace>Windows.Win32.Graphics.DirectWriteCore</Namespace>
-            <Exclude>ID2D1SimplifiedGeometrySink;_D3DCOLORVALUE</Exclude>
-        </Partition>
-    </ItemGroup>
+  <ItemGroup>
+    <DWriteHeaders Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.h"/>
+    <DWriteLibs Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.lib"/>
 
-    <ItemGroup>
-        <PackageReference Include="Microsoft.ProjectReunion.DWrite" Version="0.5.6" GeneratePathProperty="true"/>
-    </ItemGroup>
+    <ImportLibs Include="@(DWriteLibs)"/>
+    <Partition Include="main.cpp">
+      <TraverseFiles>@(DWriteHeaders)</TraverseFiles>
+      <Namespace>Windows.Win32.Graphics.DirectWriteCore</Namespace>
+      <Exclude>ID2D1SimplifiedGeometrySink;_D3DCOLORVALUE</Exclude>
+    </Partition>
+  </ItemGroup>
 
-    <Import Project="..\sdk\sdk.targets" />
+  <ItemGroup>
+    <PackageReference Include="Microsoft.ProjectReunion.DWrite" Version="0.5.6" GeneratePathProperty="true"/>
+  </ItemGroup>
+
+  <Import Project="..\sdk\sdk.targets" />
 </Project>

sources/msbuild/nuget/BuildSdk.nuspec
@@ -26,9 +26,9 @@
       <file src="tools\link.exe" target="tools"/>
       <file src="tools\tbbmalloc.dll" target="tools"/>
 
-      <file src="bin\release\netcoreapp3.1\*.deps.json" target="tools\netcoreapp3.1"/>
-      <file src="bin\release\netcoreapp3.1\*.runtimeconfig.json" target="tools\netcoreapp3.1"/>
-      <file src="bin\release\netcoreapp3.1\*.dll" target="tools\netcoreapp3.1" />
+      <file src="bin\release\net5.0\*.deps.json" target="tools\net5.0"/>
+      <file src="bin\release\net5.0\*.runtimeconfig.json" target="tools\net5.0"/>
+      <file src="bin\release\net5.0\*.dll" target="tools\net5.0" />
       <file src="scripts\InstallTools.ps1" target="scripts"/>
       <file src="scripts\ConvertMidlAttributesToSalAnnotations.ps1" target="scripts"/>
       <file src="scripts\GetVcDirPath.ps1" target="scripts"/>

commit 3ab34ff41bccbce23304713e077b42481192a761

Fix metadata SDK project, change samples to build against built nuget packages (#649)
* Add MetadataTasks.dll to Microsoft.Windows.WinmdGenerator package

Fixes #644
This probably regressed in #630

* Bring back `Project/@Sdk` attribute

* * Additional fixes to metadata sdk
* Change the samples to use built nuget packages
* Change the DoAll.ps1 script to build the packages and the samples

* Move metadata SDK samples into their own directory so that their nuget.config doesn't mess with product builds

Co-authored-by: Andrew Arnott <andrewarnott@live.com>

Directory.Build.props
@@ -15,7 +15,7 @@
     <OutputPath>$(RepoRootPath)bin\$(Configuration)</OutputPath>
     <PackageOutputPath>$(MSBuildThisFileDirectory)bin\Packages\$(Configuration)\NuGet\</PackageOutputPath>
 
-    <TaskBinDir>$(RepoRootPath)bin\$(Configuration)\netstandard2.0\</TaskBinDir>
+    <LocalTaskBinDir>$(RepoRootPath)bin\$(Configuration)\netstandard2.0\</LocalTaskBinDir>
 
     <LangVersion>9</LangVersion>
 

DoAll.ps1
@@ -2,6 +2,8 @@ param
 (
     [switch]$Clean,
 
+    [switch]$ExcludePackages,
+
     [ValidateSet("crossarch", "x64", "x86", "arm64")]
     [string]
     $arch = "crossarch"
@@ -46,6 +48,17 @@ else
 if ($LastExitCode -lt 0)
 {
     exit $LastExitCode
+}
+
+if (!$ExcludePackages)
+{
+    dotnet pack .\sources\packages.proj -c Release
+
+    .\scripts\UpdateGlobalJsonWinmdGeneratorVersion.ps1
+
+    dotnet pack .\sources\msbuild\samples\diasdk -c Release
+
+    dotnet build .\sources\msbuild\samples -c Release
 }
 
 if ($arch -eq "crossarch")

azure-pipelines.yml
@@ -120,7 +120,7 @@ jobs:
     clean: all
   dependsOn: ['scrape_x64','scrape_x86','scrape_arm64']
   variables:
-    OutputPackagesDir: $(Build.ArtifactStagingDirectory)\CreatedNugetPackages
+    OutputPackagesDir: $(Build.SourcesDirectory)\bin\Packages\Release\NuGet
   pool:
     vmImage: windows-2019
   steps:
@@ -318,23 +318,20 @@ jobs:
     condition: and(succeeded(), eq(variables['SignFiles'], 'true'), ne(variables['Build.Reason'], 'PullRequest'))
 
   - task: PowerShell@2
-    displayName: Pack metadata package
+    displayName: Pack NuGet packages
     inputs:
       targetType: inline
       script: |
-        cd $(Build.SourcesDirectory)\sources\nuget\Microsoft.Windows.SDK.Win32Metadata
-        dotnet pack -o "$(OutputPackagesDir)"
+        dotnet pack $(Build.SourcesDirectory)\sources\packages.proj -c Release
       errorActionPreference: 'continue'
       pwsh: true
 
   - task: PowerShell@2
-    displayName: Pack MSBuildSDK package
+    displayName: Update global.json with MSBuildSDK version
     inputs:
       targetType: inline
       script: |
-        cd $(Build.SourcesDirectory)\sources\msbuild\nuget
-        dotnet pack -o "$(OutputPackagesDir)"
-      errorActionPreference: 'continue'
+        $(Build.SourcesDirectory)\scripts\UpdateGlobalJsonWinmdGeneratorVersion.ps1
       pwsh: true
 
   - task: PowerShell@2
@@ -342,9 +339,16 @@ jobs:
     inputs:
       targetType: inline
       script: |
-        cd $(Build.SourcesDirectory)\sources\msbuild\DiaSdk
-        dotnet pack -o "$(OutputPackagesDir)"
+        dotnet pack $(Build.SourcesDirectory)\sources\msbuild\samples\DiaSdk -c Release
       errorActionPreference: 'continue'
+      pwsh: true
+
+  - task: PowerShell@2
+    displayName: Test MSBuildSDK with samples
+    inputs:
+      targetType: inline
+      script: |
+        dotnet build $(Build.SourcesDirectory)\sources\msbuild\samples -c Release
       pwsh: true
 
   - task: EsrpCodeSigning@1

scripts/UpdateGlobalJsonWinmdGeneratorVersion.ps1
@@ -1,0 +1,9 @@
+$rootDir = [System.IO.Path]::GetFullPath("$PSScriptRoot\..")
+$winmdGeneratorSdkDir = Join-Path $rootDir "sources\msbuild"
+$globalJson = Join-Path $rootDir "global.json"
+$winmdGeneratorSdkVersion = nbgv get-version -v NuGetPackageVersion -p $winmdGeneratorSdkDir
+$json = Get-Content $globalJson -raw | ConvertFrom-Json
+$json."msbuild-sdks"."Microsoft.Windows.WinmdGenerator" = $winmdGeneratorSdkVersion
+$json | ConvertTo-Json | Out-File $globalJson
+Write-Host "Updated $globalJson to use $winmdGeneratorSdkVersion for WinmdGenerator SDK"
+

sources/msbuild/GeneratorSampleDebug/GeneratorSampleDebug.proj
@@ -1,39 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<Project Sdk="Microsoft.Build.NoTargets">
-
-  <!-- These are for debugging purposes only. A normal project wouldn't set these -->
-  <PropertyGroup>
-    <ToolsBinDir>$(RepoRootPath)bin\$(Configuration)\net5.0</ToolsBinDir>
-    <LibToolsBinDir>$(RepoRootPath)tools</LibToolsBinDir>
-    <ScriptsDir>$(RepoRootPath)scripts</ScriptsDir>
-    <Win32MetadataScraperAssetsDir>$(RepoRootPath)generation\scraper</Win32MetadataScraperAssetsDir>
-    <Win32MetadataScraperGeneratedAssetsDir>$(RepoRootPath)generation\scraper\obj\x64</Win32MetadataScraperGeneratedAssetsDir>
-    <Win32WinmdBinDir>$(RepoRootPath)bin</Win32WinmdBinDir>
-  </PropertyGroup>
-
-  <Import Project="..\sdk\sdk.props" />
-
-  <PropertyGroup>
-    <OutputWinmd>bin\Windows.Win32.Graphics.DirectWriteCore.winmd</OutputWinmd>
-    <WinmdVersion>1.0.0.0</WinmdVersion>
-    <GenerateAssemblyVersionInfo>false</GenerateAssemblyVersionInfo>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <DWriteHeaders Condition=" '$(PkgMicrosoft_ProjectReunion_DWrite)' != '' " Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.h"/>
-    <DWriteLibs Condition=" '$(PkgMicrosoft_ProjectReunion_DWrite)' != '' " Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.lib"/>
-
-    <ImportLibs Include="@(DWriteLibs)"/>
-    <Partition Include="main.cpp">
-      <TraverseFiles>@(DWriteHeaders)</TraverseFiles>
-      <Namespace>Windows.Win32.Graphics.DirectWriteCore</Namespace>
-      <Exclude>ID2D1SimplifiedGeometrySink;_D3DCOLORVALUE</Exclude>
-    </Partition>
-  </ItemGroup>
-
-  <ItemGroup>
-    <PackageReference Include="Microsoft.ProjectReunion.DWrite" Version="0.5.6" GeneratePathProperty="true"/>
-  </ItemGroup>
-
-  <Import Project="..\sdk\sdk.targets" />
-</Project>

sources/msbuild/MetadataTasks/EmitWinmd.cs
@@ -88,14 +88,19 @@ namespace MetadataTasks
 
         protected override string GenerateResponseFileCommands()
         {
-            var builder = new CommandLineBuilder();
             var staticLibs = this.GetStaticLibs();
             if (staticLibs.Length > 0)
             {
-                builder.AppendSwitchIfNotNull("--staticLibs ", staticLibs," ");
+                StringBuilder ret = new StringBuilder("--staticLibs\n");
+                foreach (var lib in staticLibs)
+                {
+                    ret.AppendLine(lib);
+                }
+
+                return ret.ToString();
             }
 
-            return builder.ToString();
+            return null;
         }
 
         protected override int ExecuteTool(string pathToTool, string responseFileCommands, string commandLineCommands)

sources/msbuild/nuget/BuildSdk.nuspec
@@ -29,6 +29,7 @@
       <file src="bin\release\net5.0\*.deps.json" target="tools\net5.0"/>
       <file src="bin\release\net5.0\*.runtimeconfig.json" target="tools\net5.0"/>
       <file src="bin\release\net5.0\*.dll" target="tools\net5.0" />
+      <file src="bin\release\netstandard2.0\*.dll" target="tools\netstandard2.0" />
       <file src="scripts\InstallTools.ps1" target="scripts"/>
       <file src="scripts\ConvertMidlAttributesToSalAnnotations.ps1" target="scripts"/>
       <file src="scripts\GetVcDirPath.ps1" target="scripts"/>

sources/msbuild/samples/DiaSdk/DiaSdk.proj
@@ -1,16 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<Project Sdk="Microsoft.Build.NoTargets">
-  <!-- These are for debugging purposes only. A normal project wouldn't set these -->
-  <PropertyGroup>
-    <ToolsBinDir>$(RepoRootPath)bin\$(Configuration)\net5.0</ToolsBinDir>
-    <LibToolsBinDir>$(RepoRootPath)tools</LibToolsBinDir>
-    <ScriptsDir>$(RepoRootPath)scripts</ScriptsDir>
-    <Win32MetadataScraperAssetsDir>$(RepoRootPath)generation\scraper</Win32MetadataScraperAssetsDir>
-    <Win32MetadataScraperGeneratedAssetsDir>$(RepoRootPath)generation\scraper\obj\x64</Win32MetadataScraperGeneratedAssetsDir>
-    <Win32WinmdBinDir>$(RepoRootPath)bin</Win32WinmdBinDir>
-  </PropertyGroup>
-
-  <Import Project="..\sdk\sdk.props" />
+<Project Sdk="Microsoft.Windows.WinmdGenerator">
 
   <PropertyGroup>
     <OutputWinmd>$(RepoRootPath)bin\Microsoft.Dia.winmd</OutputWinmd>
@@ -47,16 +36,10 @@
     </Partition>
   </ItemGroup>
 
-  <ItemGroup>
-    <ProjectReference Include="..\MetadataTasks\MetadataTasks.csproj" />
-    <ProjectReference Include="..\..\ClangSharpSourceToWinmd\ClangSharpSourceToWinmd.csproj" />
-  </ItemGroup>
-
   <Target Name="SetNuSpecProperties" BeforeTargets="GenerateNuspec" DependsOnTargets="GetBuildVersion">
     <PropertyGroup>
       <NuspecProperties>$(NuspecProperties);OutputWinmd=$(OutputWinmd);Version=$(Version)</NuspecProperties>
     </PropertyGroup>
   </Target>
 
-  <Import Project="..\sdk\sdk.targets" />
 </Project>

sources/msbuild/samples/DiaSdk/Microsoft.Dia.Win32Metadata.nuspec
@@ -19,7 +19,7 @@
   </metadata>
 
   <files>
-      <file src="..\..\..\images\windows.png" target="images"/>
+      <file src="..\..\..\..\images\windows.png" target="images"/>
 
       <file src="$OutputWinmd$" target=""/>
       <file src="buildTransitive\**" target="buildTransitive\"/>

sources/msbuild/samples/GeneratorSampleDebug/GeneratorSampleDebug.proj
@@ -9,34 +9,32 @@
     <Win32MetadataScraperAssetsDir>$(RepoRootPath)generation\scraper</Win32MetadataScraperAssetsDir>
     <Win32MetadataScraperGeneratedAssetsDir>$(RepoRootPath)generation\scraper\obj\x64</Win32MetadataScraperGeneratedAssetsDir>
     <Win32WinmdBinDir>$(RepoRootPath)bin</Win32WinmdBinDir>
+    <TaskBinDir>$(LocalTaskBinDir)</TaskBinDir>
   </PropertyGroup>
 
-  <Import Project="..\sdk\sdk.props" />
+  <Import Project="..\..\sdk\sdk.props" />
 
   <PropertyGroup>
-    <OutputWinmd>bin\Microsoft.Web.WebView2.winmd</OutputWinmd>
-    <WinmdVersion>1.0.864.35</WinmdVersion>
+    <OutputWinmd>bin\Windows.Win32.Graphics.DirectWriteCore.winmd</OutputWinmd>
+    <WinmdVersion>1.0.0.0</WinmdVersion>
     <GenerateAssemblyVersionInfo>false</GenerateAssemblyVersionInfo>
   </PropertyGroup>
 
   <ItemGroup>
-    <WebView2Headers Condition=" '$(PkgMicrosoft_Web_WebView2)' != '' " Include="$(PkgMicrosoft_Web_WebView2)\**\*.h"/>
-    <WebView2Libs Condition=" '$(PkgMicrosoft_Web_WebView2)' != '' " Include="$(PkgMicrosoft_Web_WebView2)\**\*.dll.lib"/>
+    <DWriteHeaders Condition=" '$(PkgMicrosoft_ProjectReunion_DWrite)' != '' " Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.h"/>
+    <DWriteLibs Condition=" '$(PkgMicrosoft_ProjectReunion_DWrite)' != '' " Include="$(PkgMicrosoft_ProjectReunion_DWrite)\**\*.lib"/>
 
-    <ImportLibs Include="@(WebView2Libs)">
-      <StaticLibs>WebView2Loader=WebView2LoaderStatic</StaticLibs>
-    </ImportLibs>
-
+    <ImportLibs Include="@(DWriteLibs)"/>
     <Partition Include="main.cpp">
-      <TraverseFiles>@(WebView2Headers)</TraverseFiles>
-      <Namespace>Microsoft.Web.WebView2.Win32</Namespace>
-      <Exclude>CoreWebView2EnvironmentOptions</Exclude>
+      <TraverseFiles>@(DWriteHeaders)</TraverseFiles>
+      <Namespace>Windows.Win32.Graphics.DirectWriteCore</Namespace>
+      <Exclude>ID2D1SimplifiedGeometrySink;_D3DCOLORVALUE</Exclude>
     </Partition>
   </ItemGroup>
 
   <ItemGroup>
-    <PackageReference Include="Microsoft.Web.WebView2" Version="$(WinmdVersion)" GeneratePathProperty="true"/>
+    <PackageReference Include="Microsoft.ProjectReunion.DWrite" Version="0.5.6" GeneratePathProperty="true"/>
   </ItemGroup>
 
-  <Import Project="..\sdk\sdk.targets" />
+  <Import Project="..\..\sdk\sdk.targets" />
 </Project>

sources/msbuild/samples/WebView2Sample/WebView2Sample.proj
@@ -1,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Project Sdk="Microsoft.Windows.WinmdGenerator">
+
+  <PropertyGroup>
+    <OutputWinmd>bin\Microsoft.Web.WebView2.winmd</OutputWinmd>
+    <WinmdVersion>1.0.864.35</WinmdVersion>
+    <GenerateAssemblyVersionInfo>false</GenerateAssemblyVersionInfo>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <WebView2Headers Condition=" '$(PkgMicrosoft_Web_WebView2)' != '' " Include="$(PkgMicrosoft_Web_WebView2)\**\*.h"/>
+    <WebView2Libs Condition=" '$(PkgMicrosoft_Web_WebView2)' != '' " Include="$(PkgMicrosoft_Web_WebView2)\**\*.dll.lib"/>
+
+    <ImportLibs Include="@(WebView2Libs)">
+      <StaticLibs>WebView2Loader=WebView2LoaderStatic</StaticLibs>
+    </ImportLibs>
+
+    <Partition Include="main.cpp">
+      <TraverseFiles>@(WebView2Headers)</TraverseFiles>
+      <Namespace>Microsoft.Web.WebView2.Win32</Namespace>
+      <Exclude>CoreWebView2EnvironmentOptions</Exclude>
+    </Partition>
+  </ItemGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Microsoft.Web.WebView2" Version="$(WinmdVersion)" GeneratePathProperty="true"/>
+  </ItemGroup>
+
+</Project>

sources/msbuild/samples/nuget.config
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <add key="LocalPackages" value="..\..\..\bin\Packages\Release\NuGet" />
+  </packageSources>
+  <disabledPackageSources>
+    <clear />
+  </disabledPackageSources>
+</configuration>

sources/msbuild/samples/samples.proj
@@ -1,0 +1,12 @@
+<Project Sdk="Microsoft.Build.Traversal">
+  <PropertyGroup>
+    <BuildInParallel>false</BuildInParallel>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <ProjectReference Include="DiaSdk\DiaSdk.proj" />
+    <ProjectReference Include="GeneratorSample\GeneratorSample.proj" />
+    <ProjectReference Include="GeneratorSampleDebug\GeneratorSampleDebug.proj" />
+    <ProjectReference Include="WebView2Sample\WebView2Sample.proj" />
+  </ItemGroup>
+</Project>

sources/msbuild/sdk/sdk.props
@@ -1,13 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <Project>
   <PropertyGroup>
+    <MicrosoftNoTargetsSdkImported Condition="'$(UsingMicrosoftNoTargetsSdk)' != 'true'">true</MicrosoftNoTargetsSdkImported>
+  </PropertyGroup>
+
+  <Import Condition="'$(UsingMicrosoftNoTargetsSdk)' != 'true'" Sdk="Microsoft.Build.NoTargets" Project="sdk.props"/>
+
+  <PropertyGroup>
     <TargetFramework>net5.0</TargetFramework>
     <TargetPlatformIdentifier>Windows</TargetPlatformIdentifier>
   </PropertyGroup>
 
   <PropertyGroup>
     <Win32MetadataSdkRoot>$([System.IO.Path]::GetFullPath('$(MSBuildThisFileDirectory)..'))</Win32MetadataSdkRoot>
-    <TaskBinDir Condition="'$(TaskBinDir)' == ''">$(Win32MetadataSdkRoot)\tools\net5.0</TaskBinDir>
+    <TaskBinDir Condition="'$(TaskBinDir)' == ''">$(Win32MetadataSdkRoot)\tools\netstandard2.0</TaskBinDir>
     <ToolsBinDir Condition="'$(ToolsBinDir)' == ''">$(Win32MetadataSdkRoot)\tools\net5.0</ToolsBinDir>
     <LibToolsBinDir Condition="'$(LibToolsBinDir)' == ''">$(Win32MetadataSdkRoot)\tools</LibToolsBinDir>
     <ScriptsDir Condition="'$(ScriptsDir)' == ''">$(Win32MetadataSdkRoot)\scripts</ScriptsDir>

sources/msbuild/sdk/sdk.targets
@@ -62,4 +62,6 @@
   <Target Name = "Clean">
     <RemoveDir Directories="obj;bin" />
   </Target>
+
+  <Import Condition="'$(MicrosoftNoTargetsSdkImported)' == 'true'" Sdk="Microsoft.Build.NoTargets" Project="sdk.targets"/>
 </Project>

sources/packages.proj
@@ -1,0 +1,10 @@
+<Project Sdk="Microsoft.Build.Traversal">
+  <PropertyGroup>
+    <BuildInParallel>false</BuildInParallel>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <ProjectReference Include="nuget\Microsoft.Windows.SDK.Win32Metadata\Win32MetadataNuget.proj" />
+    <ProjectReference Include="msbuild\nuget\BuildSdk.proj" />
+  </ItemGroup>
+</Project>