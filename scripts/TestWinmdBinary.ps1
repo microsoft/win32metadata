bb40933f819082d8c78906f8d54bc3ab3e38e9b6
Steve Otteson <sotteson@microsoft.com>
2021-09-23 20:17:08 -0700

    Bad variable in script

--- scripts/TestWinmdBinary.ps1
+++ scripts/TestWinmdBinary.ps1
@@ -21,7 +21,7 @@ $winmdUtilsPathBin = "$metadataToolsBin\WinmdUtils.dll"
 
 $failed = $false
 
-Write-Output "`nLooking for duplicate imports in $outputWinmdFileName..."
+Write-Output "`nLooking for duplicate imports in $winmdPath..."
 Write-Output "Calling: dotnet $winmdUtilsPathBin showDuplicateImports --winmd $winmdPath"
 & dotnet $winmdUtilsPathBin showDuplicateImports --winmd $winmdPath
 if (!$failed -and $LastExitCode -lt 0)
@@ -29,7 +29,7 @@ if (!$failed -and $LastExitCode -lt 0)
     $failed = $true
 }
 
-Write-Output "`nLooking for duplicate types in $outputWinmdFileName..."
+Write-Output "`nLooking for duplicate types in $winmdPath..."
 Write-Output "Calling: dotnet $winmdUtilsPathBin showDuplicateTypes --winmd $winmdPath"
 & dotnet $winmdUtilsPathBin showDuplicateTypes --winmd $winmdPath
 if (!$failed -and $LastExitCode -lt 0)
@@ -37,7 +37,7 @@ if (!$failed -and $LastExitCode -lt 0)
     $failed = $true
 }
 
-Write-Output "`nLooking for duplicate constants in $outputWinmdFileName..."
+Write-Output "`nLooking for duplicate constants in $winmdPath..."
 Write-Output "Calling: dotnet $winmdUtilsPathBin showDuplicateConstants --winmd $winmdPath"
 & dotnet $winmdUtilsPathBin showDuplicateConstants --winmd $winmdPath
 if (!$failed -and $LastExitCode -lt 0)
@@ -46,7 +46,7 @@ if (!$failed -and $LastExitCode -lt 0)
 }
 
 $allowedEmptyDelegatesFileName = "$rootDir\tests\emptyDelegatesAllowList.rsp"
-Write-Output "`nLooking for empty delegates in $outputWinmdFileName..."
+Write-Output "`nLooking for empty delegates in $winmdPath..."
 Write-Output "Calling: dotnet $winmdUtilsPathBin showEmptyDelegates --winmd $winmdPath @$allowedEmptyDelegatesFileName"
 & dotnet $winmdUtilsPathBin showEmptyDelegates --winmd $winmdPath @$allowedEmptyDelegatesFileName
 if (!$failed -and $LastExitCode -lt 0)
@@ -55,7 +55,7 @@ if (!$failed -and $LastExitCode -lt 0)
 }
 
 $pointersToDelegatesAllowListFileName = "$rootDir\tests\pointersToDelegatesAllowList.rsp"
-Write-Output "`nLooking for pointers to delegates in $outputWinmdFileName..."
+Write-Output "`nLooking for pointers to delegates in $winmdPath..."
 Write-Output "Calling: dotnet $winmdUtilsPathBin showPointersToDelegates --winmd $winmdPath @$pointersToDelegatesAllowListFileName"
 & dotnet $winmdUtilsPathBin showPointersToDelegates --winmd $winmdPath @$pointersToDelegatesAllowListFileName
 if (!$failed -and $LastExitCode -lt 0)
