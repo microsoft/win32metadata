15eda95fdeb386ca0e49c282c37d9b9496ff7058
Rafael Rivera <rafael@withinwindows.com>
2022-03-28 15:08:07 -0700

    Add InvalidHandleValueAttribute support (#853)
    * Add InvalidHandleValueAttribute, tests
    
    * Add InvalidHandleValue attribute to FindVolumeHandle
    
    * Address feedback
    
    * Support multiple InvalidHandleValues
    
    * Fix build

---
+++ sources/ClangSharpSourceToWinmd/AutoType.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace ClangSharpSourceToWinmd
+{
+    public class AutoType
+    {
+        public string Namespace { get; set; }
+        public string Name { get; set; }
+        public string ValueType { get; set; }
+        public string CloseApi { get; set; }
+        public string AlsoUsableFor { get; set; }
+        public long[] InvalidHandleValues { get; set; }
+    }
+}
---
+++ sources/Win32MetadataInterop/InvalidHandleValueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Windows.Win32.Interop
+{
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = true, Inherited = true)]
+    public class InvalidHandleValueAttribute : Attribute
+    {
+        public InvalidHandleValueAttribute(long value) => this.Value = value;
+        public long Value { get; }
+    }
+}
