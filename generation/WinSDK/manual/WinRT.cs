0b92520ae12b649520b1f9997a59be7baca929a3
Rafael Rivera <rafael@withinwindows.com>
2022-04-16 13:42:38 -0700

    Provide manual definition of HSTRING_HEADER

--- generation/WinSDK/manual/WinRT.cs
+++ generation/WinSDK/manual/WinRT.cs
@@ -9,4 +9,13 @@ namespace Windows.Win32.System.WinRT
     {
         public long value;
     }
+
+    public struct HSTRING_HEADER
+    {
+        public uint flags;
+        public uint length;
+        public uint padding1;
+        public uint padding2;
+        public IntPtr data;
+    }
 }
