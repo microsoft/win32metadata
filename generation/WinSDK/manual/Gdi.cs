1fc8ed8f0d3ecef6c23b4bc8402d35d65a2cc7f0
Rafael Rivera <rafael@withinwindows.com>
2022-05-25 00:01:36 -0700

    Define ROP_CODE as bit field (#943)

--- generation/WinSDK/manual/Gdi.cs
+++ generation/WinSDK/manual/Gdi.cs
@@ -69,27 +69,6 @@ namespace Windows.Win32.Graphics.Gdi
         OBJ_COLORSPACE = 14
     }
 
-    public enum ROP_CODE : uint
-    {
-        SRCCOPY = 0x00CC0020,
-        SRCPAINT = 0x00EE0086,
-        SRCAND = 0x008800C6,
-        SRCINVERT = 0x00660046,
-        SRCERASE = 0x00440328,
-        NOTSRCCOPY = 0x00330008,
-        NOTSRCERASE = 0x001100A6,
-        MERGECOPY = 0x00C000CA,
-        MERGEPAINT = 0x00BB0226,
-        PATCOPY = 0x00F00021,
-        PATPAINT = 0x00FB0A09,
-        PATINVERT = 0x005A0049,
-        DSTINVERT = 0x00550009,
-        BLACKNESS = 0x00000042,
-        WHITENESS = 0x00FF0062,
-        NOMIRRORBITMAP = 0x80000000,
-        CAPTUREBLT = 0x40000000
-    }
-
     public enum DIB_USAGE : uint
     {
         DIB_RGB_COLORS = 0,
