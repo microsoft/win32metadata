663ae1fd98ae2b727c407f9d4855e07b85778890
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-08-02 17:04:13 -0700

    Fixed #992.

---
+++ generation/WinSDK/manual/Power.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Interop;
+
+namespace Windows.Win32.System.Power
+{
+    public enum POWER_COOLING_MODE : ushort
+    {
+        PO_TZ_ACTIVE,
+        PO_TZ_PASSIVE,
+        PO_TZ_INVALID_MODE
+    }
+
+    public partial struct PROCESSOR_POWER_INFORMATION
+    {
+        public ulong Number;
+        
+        public ulong MaxMhz;
+        
+        public ulong CurrentMhz;
+        
+        public ulong MhzLimit;
+        
+        public ulong MaxIdleState;
+        
+        public ulong CurrentIdleState;
+    }
+
+    public partial struct SYSTEM_POWER_INFORMATION
+    {
+        public ulong MaxIdlenessAllowed;
+        
+        public ulong Idleness;
+        
+        public ulong TimeRemaining;
+        
+        public POWER_COOLING_MODE CoolingMode;
+    }
+}
