68b14dfbeb5da4f05b5e3634705b071dcaaf617e
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-08-04 00:30:33 -0700

    Fixed #995.

--- generation/WinSDK/manual/FileSystem.cs
+++ generation/WinSDK/manual/FileSystem.cs
@@ -101,7 +101,14 @@ namespace Windows.Win32.Storage.FileSystem
 
         FILE_WRITE_ATTRIBUTES = 0x0100,
 
+        DELETE = 0x00010000,
+        
         READ_CONTROL = 0x00020000,
+
+        WRITE_DAC = 0x00040000,
+
+        WRITE_OWNER = 0x00080000,
+        
         SYNCHRONIZE = 0x00100000,
 
         STANDARD_RIGHTS_REQUIRED = 0x000F0000,
