c9e348a3c4149fa73431f303a8f22865f2a85ce4
Steve Otteson <sotteson@microsoft.com>
2022-08-29 14:27:18 -0700

    Use a common property for the last release version. (#1186)
    Add a script to edit that property and clear the known changes.

---
+++ scripts/Edit-LastReleaseVersion.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$LastReleaseVersion
+)
+
+
+$dirBuildPropsFile = Join-Path $PSScriptRoot "..\Directory.Build.Props"
+
+Write-Verbose "Updating $dirBuildPropsFile..."
+
+$dirBuildPropsXml = New-Object xml
+$dirBuildPropsXml.PreserveWhitespace = $true
+$dirBuildPropsXml.Load($dirBuildPropsFile)
+
+$dirBuildPropsXml.Project.PropertyGroup.LastWin32MetadataReleaseVersion = $LastReleaseVersion
+$dirBuildPropsXml.Save($dirBuildPropsFile)
+
+. "$PSScriptRoot\CommonUtils.ps1"
+$changesFile = Get-ChangesSinceLastReleaseFile
+Write-Verbose "Clearing contents of $changesFile..."
+
+Clear-Content -Path $changesFile
+
