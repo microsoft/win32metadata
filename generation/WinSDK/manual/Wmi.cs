d3e5f7098c3420664e7a179ca691f877c89813c3
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-09-21 13:59:46 -0700

    Fixed Rust usability issues.
    Fixed #1263.
    Fixed #1265.
    Fixed #1266.

---
+++ generation/WinSDK/manual/Wmi.cs
@@ -0,0 +1,9 @@
+namespace Windows.Win32.System.Wmi
+{
+    public static partial class Apis
+    {
+        public const int WBEM_NO_WAIT = (int)0;
+
+        public const int WBEM_INFINITE = unchecked((int)0xffffffff);
+    }
+}
