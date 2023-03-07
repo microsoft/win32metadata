73ca3d911f3665831c7efa22b70ebaf256dc7237
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2023-03-07 12:33:10 -0800

    Fixed #433.

---
+++ sources/Win32MetadataInterop/StructSizeFieldAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows.Win32.Interop
+{
+    public class StructSizeFieldAttribute : Attribute
+    {
+        public StructSizeFieldAttribute(string field)
+        {
+
+        }
+    }
+}
