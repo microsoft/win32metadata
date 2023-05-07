95e2a2e237ebf344e10dc5590cbaf8fbb19f6540
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2023-05-07 09:51:06 -0700

    Add word boundary to index and TOC patterns.

--- apidocs/ScrapeDocs/Program.cs
+++ apidocs/ScrapeDocs/Program.cs
@@ -49,7 +49,7 @@ internal class Program
         { @"/ext/win32/desktop-src", @"https://docs.microsoft.com/windows/win32/" },
     };
 
-    private static readonly string ExcludedContentPattern = @"(ADSchema|CIMWin32Prov|delivery_optimization|DMWmiBridgeProv|gdiplus|HyperV_v2|Midl\b|SecAuthZ|TermServ|WmiCoreProv|WmiSdk|index\.md|TOC\.md)";
+    private static readonly string ExcludedContentPattern = @"(ADSchema|CIMWin32Prov|delivery_optimization|DMWmiBridgeProv|gdiplus|HyperV_v2|Midl\b|SecAuthZ|TermServ|WmiCoreProv|WmiSdk|\bindex\.md|\bTOC\.md)";
 
     private readonly string contentBasePaths;
     private readonly string outputPath;
