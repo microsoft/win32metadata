c0b57011eb7443f7de4e61adb1940f4875f53d02
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2023-07-18 07:15:02 -0700

    Bug: Missing definitions for DEVPROP_BOOLEAN. Fixed #1621.

---
+++ generation/WinSDK/manual/Devices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.Devices.Properties
+{
+    public static unsafe partial class Apis
+    {
+
+        [NativeTypeName("DEVPROP_BOOLEAN")]
+        public const char DEVPROP_TRUE  = unchecked((char)-1);
+
+        [NativeTypeName("DEVPROP_BOOLEAN")]
+        public const char DEVPROP_FALSE = (char)0;
+    }
+}
