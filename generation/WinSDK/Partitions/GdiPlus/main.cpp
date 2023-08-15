01d5c22b1ef64113b585a92c102ca25f2d5c822d
Rafael Rivera <rafael@withinwindows.com>
2023-08-14 22:37:08 -0700

    Initial support for GDI+

---
+++ generation/WinSDK/Partitions/GdiPlus/main.cpp
@@ -0,0 +1,14 @@
+#include "intrinfix.h"
+#include "windows.fixed.h"
+#include <sdkddkver.h>
+
+#include <ddraw.h>
+#include <gdiplusenums.h>
+#include <gdiplustypes.h>
+#include <gdiplusmetaheader.h>
+#include <gdipluspixelformats.h>
+#include <gdipluscolor.h>
+#include <gdipluscolormatrix.h>
+#include <gdiplusimaging.h>
+#include <gdiplusgpstubs.h>
+#include <gdiplusflat.h>
