e49ce2191e55dccfcc2e6fb2b5a52ed775cb2c94
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2023-11-13 09:29:39 -0800

    Missing EXCEPTION_CONTINUE_EXECUTION, etc. Fixed #1731.

---
+++ generation/WinSDK/manual/Debug.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.System.Diagnostics.Debug
+{
+    public static unsafe partial class Apis
+    {
+        public const int EXCEPTION_EXECUTE_HANDLER = 1;
+        public const int EXCEPTION_CONTINUE_SEARCH = 0;
+        public const int EXCEPTION_CONTINUE_EXECUTION = -1;
+    }
+}
