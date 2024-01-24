8f1e5cc2b486b74cf2c1eab8cc93f87712e2ea54
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2024-01-24 10:08:25 -0800

    `CLSID_DxcUtils` is not included in metadata. Fixed #1812.

---
+++ generation/WinSDK/manual/Dxc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.Graphics.Direct3D.Dxc
+{
+    public static unsafe partial class Apis
+    {
+        public static readonly Guid CLSID_DxcUtils = new Guid("6245d6af-66e0-48fd-80b4-4d271796748c");
+    }
+}
