bc17b1a0fb0b046cef92a791750a3befc9d5cf99
Geoffrey Trousdale <getrou@microsoft.com>
2024-03-26 21:11:15 -0400

    Install tools for other tasks as well

--- scripts/DoPackages.ps1
+++ scripts/DoPackages.ps1
@@ -19,16 +19,5 @@ ThrowOnNativeProcessError
 
 dotnet clean "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 
-
-# & "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
-
-# dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
-
-# dir -S "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk"
-
-# Get-Content "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk\obj\dia2.idl"
-
-# dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
-
 dotnet pack "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 ThrowOnNativeProcessError
