dcf2c5303ba94beddaef70d480c6f0eccfd687b3
Geoffrey Trousdale <getrou@microsoft.com>
2024-03-26 20:30:19 -0400

    Remove extra steps in DoPackages.ps1

--- scripts/DoPackages.ps1
+++ scripts/DoPackages.ps1
@@ -20,15 +20,15 @@ ThrowOnNativeProcessError
 dotnet clean "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 
 
-& "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+# & "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 
-dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
+# dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 
-dir -S "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk"
+# dir -S "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk"
 
-Get-Content "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk\obj\dia2.idl"
+# Get-Content "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk\obj\dia2.idl"
 
-dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
+# dotnet build "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 
 dotnet pack "$PSScriptRoot\..\sources\GeneratorSdk\samples\diasdk" -c Release
 ThrowOnNativeProcessError
