73572ec9b726bbdef153ffe7abea7955b99a5638
Rafael Rivera <rafael@withinwindows.com>
2024-05-09 15:03:31 -0700

    Add missing lang/locale default constants

---
+++ generation/WinSDK/manual/Globalization.cs
@@ -0,0 +1,10 @@
+namespace Windows.Win32.Globalization
+{
+    public static partial class Apis
+    {
+        public const int LANG_SYSTEM_DEFAULT = 2048; // (MAKELANGID(LANG_NEUTRAL, SUBLANG_SYS_DEFAULT))
+        public const int LANG_USER_DEFAULT = 1024; // (MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT))
+        public const int LOCALE_SYSTEM_DEFAULT = 2048; // (MAKELCID(LANG_SYSTEM_DEFAULT, SORT_DEFAULT))
+        public const int LOCALE_USER_DEFAULT  = 1024; // (MAKELCID(LANG_USER_DEFAULT, SORT_DEFAULT))
+    }
+}
