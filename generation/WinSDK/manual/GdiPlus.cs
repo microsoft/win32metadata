156fc3566bab43db55e543a5d0debf8a248c8ec3
Rafael Rivera <rafael@withinwindows.com>
2024-05-09 21:40:30 -0700

    Make GDI+ private types opaque

---
+++ generation/WinSDK/manual/GdiPlus.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.Graphics.GdiPlus
+{
+    public struct GpGraphics;
+    public struct GpBrush;
+    public struct GpTexture
+    public struct GpSolidFill
+    public struct GpLineGradient
+    public struct GpPathGradient
+    public struct GpHatch
+    public struct GpPen;
+    public struct GpCustomLineCap;
+    public struct GpAdjustableArrowCap;
+    public struct GpImage;
+    public struct GpBitmap;
+    public struct GpMetafile;
+    public struct GpImageAttributes;
+    public struct GpPath;
+    public struct GpRegion;
+    public struct GpPathIterator;
+    public struct GpFontFamily;
+    public struct GpFont;
+    public struct GpStringFormat;
+    public struct GpFontCollection;
+    public struct GpInstalledFontCollection;
+    public struct GpPrivateFontCollection;
+    public struct GpCachedBitmap;
+}
