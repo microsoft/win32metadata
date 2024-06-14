c2b7ea95b5ae31b76db8f45bbe353b48a58a65f7
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2024-06-14 12:02:30 -0700

    NCRYPT_SSL_FUNCTION_TABLE/SslGetCipherSuitePRFHashAlgorithmFn doesn't exist. Fixed #1925.

--- generation/WinSDK/manual/Security.Cryptography.cs
+++ generation/WinSDK/manual/Security.Cryptography.cs
@@ -4,11 +4,16 @@ using Windows.Win32.Foundation.Metadata;
 
 namespace Windows.Win32.Security.Cryptography
 {
+    // Manually defined due to missing unsafe modifier during codegen pass
+    // https://github.com/microsoft/win32metadata/issues/1914
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    [return:NativeTypeName("SECURITY_STATUS")]
+    public unsafe delegate HRESULT SslGetCipherSuitePRFHashAlgorithmFn([NativeTypeName("NCRYPT_PROV_HANDLE")][In]UIntPtr hSslProvider, [NativeTypeName("DWORD")][In]uint dwProtocol, [NativeTypeName("DWORD")][In]uint dwCipherSuite, [NativeTypeName("DWORD")][In]uint dwKeyType, [NativeTypeName("WCHAR[64]")][NativeArrayInfo(CountConst = 64),Out]ushort* szPRFHash, [NativeTypeName("DWORD")][In]uint dwFlags);
+
     public static unsafe partial class Apis
     {
         // Manually defined due to missing header definitions
         // https://github.com/microsoft/win32metadata/issues/1605
-
         [DllImport("BCryptPrimitives.dll", CallingConvention = CallingConvention.StdCall, ExactSpelling = true)]
         [return: NativeTypeName("BOOL")]
         public static extern int SystemPrng([NativeTypeName("PUCHAR")][MemorySize(BytesParamIndex=1)][Out] byte* pbRandomData, [NativeTypeName("SIZE_T")] nuint cbRandomData);
@@ -16,12 +21,5 @@ namespace Windows.Win32.Security.Cryptography
         [DllImport("BCryptPrimitives.dll", CallingConvention = CallingConvention.StdCall, ExactSpelling = true)]
         [return: NativeTypeName("BOOL")]
         public static extern int ProcessPrng([NativeTypeName("PBYTE")][MemorySize(BytesParamIndex=1)][Out] byte* pbData, [NativeTypeName("SIZE_T")] nuint cbData);
-
-        // Manually defined due to missing unsafe modifier during codegen pass
-        // https://github.com/microsoft/win32metadata/issues/1914
-
-        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
-        [return:NativeTypeName("SECURITY_STATUS")]
-        public delegate HRESULT SslGetCipherSuitePRFHashAlgorithmFn([NativeTypeName("NCRYPT_PROV_HANDLE")][In]UIntPtr hSslProvider, [NativeTypeName("DWORD")][In]uint dwProtocol, [NativeTypeName("DWORD")][In]uint dwCipherSuite, [NativeTypeName("DWORD")][In]uint dwKeyType, [NativeTypeName("WCHAR[64]")][NativeArrayInfo(CountConst = 64),Out]ushort* szPRFHash, [NativeTypeName("DWORD")][In]uint dwFlags);
     }
 }
