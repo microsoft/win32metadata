04d03e29b4382227e4516763e8753b671d86fec3
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2024-07-03 10:04:08 -0700

    Added partition files.

---
+++ generation/WinSDK/Partitions/Nfc/main.cpp
@@ -0,0 +1,10 @@
+#include "intrinfix.h"
+
+#include "windows.fixed.h"
+#include <sdkddkver.h>
+
+#include <devpropdef.h>
+#include <winioctl.h>
+#include <nfcsedev.h>
+#include <nfcdtadev.h>
+#include <nfcradiodev.h>
---
+++ generation/WinSDK/Partitions/Nfp/main.cpp
@@ -0,0 +1,8 @@
+#include "intrinfix.h"
+
+#include "windows.fixed.h"
+#include <sdkddkver.h>
+
+#include <devpropdef.h>
+#include <winioctl.h>
+#include <nfpdev.h>
