82f5eeb67cc884d235d6a50f8bb2f28af410538a
Rafael Rivera <rafael@withinwindows.com>
2024-08-28 13:06:06 -0700

    Add constants

---
+++ generation/WinSDK/manual/Ndis.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.NetworkManagement.Ndis
+{
+    public static unsafe partial class Apis
+    {
+        public const uint NET_IF_COMPARTMENT_ID_UNSPECIFIED = 0u;
+        public const uint NET_IF_COMPARTMENT_ID_PRIMARY = 1u;
+    }
+}
