7fcbbb1d416bc09321b1f8c86738f3d09ddef568
Geoffrey Trousdale <getrou@microsoft.com>
2024-09-10 10:41:06 -0400

    Add verbose nuget restore

--- scripts/CommonUtils.ps1
+++ scripts/CommonUtils.ps1
@@ -74,6 +74,7 @@ function Install-BuildTools
         & dotnet clean "$rootDir\buildtools"
     }
 
+    & dotnet restore "$rootDir\buildtools" --verbosity detailed 
     & dotnet build "$rootDir\buildtools" -c Release "-bl:$PSScriptRoot\..\bin\logs\buildtools.binlog"
     ThrowOnNativeProcessError
 
