66962fb1d0d26e18320ac17a9fb8c05f8deb4178
Rafael Rivera <rafael@withinwindows.com>
2024-11-11 13:07:52 -0800

    Add additional device partitions (#2019)

---
+++ generation/WinSDK/Partitions/Devices.Cdrom/main.cpp
@@ -0,0 +1,5 @@
+#include "intrinfix.h"
+#include "windows.fixed.h"
+#include <sdkddkver.h>
+#include <devioctl.h>
+#include <ntddcdrm.h>
---
+++ generation/WinSDK/manual/Devices.Dvd.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation.Metadata;
+
+namespace Windows.Win32.Devices.Dvd
+{
+    public static unsafe partial class Apis
+    {
+        public const int IOCTL_DVD_BASE = 0x00000033;
+    }
+}
